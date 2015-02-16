2394cd854a0272b8345e78a203722fac8ba593cd
Jalal Albasri <jalal.albasri@gmail.com>
2015-02-16 12:24:36 +0300

    Fixed server error not finding self and nsfw

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,9 +25,17 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
 
+app.use('/nsfw', function(req, res) {
+    res.sendFile(__dirname + '/public/images/nsfw.jpg');
+});
+app.use('/self', function(req, res) {
+    res.sendFile(__dirname + '/public/images/self.jpg');
+});
+
 app.use('/api', api);
 app.use('/', routes);
 
+console.log("[APP] Env: " + app.get('env'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,22 +51,25 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
+        console.log('the error');
+        console.error(err.message);
+        console.error(err);
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
             error: err
         });
     });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+} else {
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
     });
-});
+}
 
 module.exports = app;
