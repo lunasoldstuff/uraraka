46c3cf2985d60e17f9ce4137867583d05963c0e7
Jalal Albasri <jalal.albasri@gmail.com>
2015-03-10 12:13:36 +0300

    Changed color scheme to better match reddit

--- public/javascript/ng-app/app.js
+++ public/javascript/ng-app/app.js
@@ -3,47 +3,78 @@
 /* App Module */
 
 var redditPlusApp = angular.module('redditPlusApp', [
-  'ngRoute',
-  'ngMaterial',
-  'ngAnimate',
-  'ngSanitize',
-  'linkify',
-  'angularMoment',
-  'redditPlusControllers',
-  'redditPlusFilters',
-  'redditPlusDirectives',
-  'redditPlusServices'
+	'ngRoute',
+	'ngMaterial',
+	'ngAnimate',
+	'ngSanitize',
+	'linkify',
+	'angularMoment',
+	'redditPlusControllers',
+	'redditPlusFilters',
+	'redditPlusDirectives',
+	'redditPlusServices'
 ]);
 
 redditPlusApp.constant('angularMomentConfig', {
-  preprocess: 'unix',
-  timezone: 'utc'
+	preprocess: 'unix',
+	timezone: 'utc'
 });
 
 redditPlusApp.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $routeProvider.
-      when('/r/:sub/:sort', {
-        templateUrl: 'partials/subredditPosts',
-        controller: 'subredditPostsSortCtrl'
-      }).
-      when('/r/:sub', {
-        templateUrl: 'partials/subredditPosts',
-        controller: 'subredditPostsCtrl'
-      }).
-      when('/', {
-        templateUrl: 'partials/subredditPosts',
-        controller: 'indexCtrl'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-      $locationProvider.html5Mode(true);
-  }]);
+	function($routeProvider, $locationProvider) {
+		$routeProvider.
+			when('/r/:sub/:sort', {
+				templateUrl: 'partials/subredditPosts',
+				controller: 'subredditPostsSortCtrl'
+			}).
+			when('/r/:sub', {
+				templateUrl: 'partials/subredditPosts',
+				controller: 'subredditPostsCtrl'
+			}).
+			when('/', {
+				templateUrl: 'partials/subredditPosts',
+				controller: 'indexCtrl'
+			}).
+			otherwise({
+				redirectTo: '/'
+			});
+			$locationProvider.html5Mode(true);
+	}]);
 
 
 
 redditPlusApp.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .backgroundPalette('grey');
+	$mdThemingProvider.theme('default')
+		.primaryPalette('blue')
+		// If you specify less than all of the keys, it will inherit from the
+		// default shades
+		.accentPalette('deep-orange');
 });
+
+
+/*
+	Doesnt work must have all colors defined.
+ */
+// redditPlusApp.config(function($mdThemingProvider) {
+//   $mdThemingProvider.definePalette('redditBlue', {
+//     '400': 'eff7ff',
+//     '500': 'cee3f8',
+//     '600': '336699',
+//     'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+//                                         // on this palette should be dark or light
+//     'contrastDarkColors': ['600'],
+//     'contrastLightColors': ['400']    // could also specify this if default was 'dark'
+//   });
+
+//   $mdThemingProvider.definePalette('redditOrange', {
+//     '400': 'ff8b60',
+//     '500': 'ff4500',
+//     '600': 'ff5700',
+//     'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+//                                         // on this palette should be dark or light
+//     'contrastDarkColors': ['600'],
+//     'contrastLightColors': ['400']    // could also specify this if default was 'dark'
+//   });
+//   $mdThemingProvider.theme('default')
+//     .primaryPalette('redditBlue').accentPalette('redditOrange');
+// });
