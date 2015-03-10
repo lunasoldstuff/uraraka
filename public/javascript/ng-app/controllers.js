03a979a152092636f5b96fa369d87b26ec7eedde
Jalal Albasri <jalal.albasri@gmail.com>
2015-03-10 11:12:32 +0300

    improved handling of imgur url with /gallery/id/new

--- public/javascript/ng-app/controllers.js
+++ public/javascript/ng-app/controllers.js
@@ -10,20 +10,20 @@ var redditPlusControllers = angular.module('redditPlusControllers', []);
  */
 redditPlusControllers.controller('AppCtrl', ['$scope', '$timeout', '$mdSidenav', '$log',
   function($scope, $timeout, $mdSidenav, $log) {
-    $scope.toggleLeft = function() {
-      $log.log('toggleLeft()');
-      $mdSidenav('left').toggle();
-    };
+	$scope.toggleLeft = function() {
+	  $log.log('toggleLeft()');
+	  $mdSidenav('left').toggle();
+	};
 
-    $scope.close = function() {
-      $mdSidenav('left').close();
-    };
+	$scope.close = function() {
+	  $mdSidenav('left').close();
+	};
   }
 ]);
 
 redditPlusControllers.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
   $scope.close = function() {
-    $mdSidenav('left').close();
+	$mdSidenav('left').close();
   };
 });
 
@@ -32,19 +32,19 @@ redditPlusControllers.controller('LeftCtrl', function($scope, $timeout, $mdSiden
  */
 redditPlusControllers.controller('toolbarCtrl', ['$scope', '$log', 'titleChangeService',
   function($scope, $log, titleChangeService) {
-    $scope.toolbarTitle = 'reddit+';
-    $scope.$on('handleTitleChange', function(e, d) {
-      $scope.toolbarTitle = titleChangeService.title;
-    });
+	$scope.toolbarTitle = 'reddit+';
+	$scope.$on('handleTitleChange', function(e, d) {
+	  $scope.toolbarTitle = titleChangeService.title;
+	});
   }
 ]);
 
 redditPlusControllers.controller('tabsCtrl', ['$scope', '$log', 'subredditService',
   function($scope, $log, subredditService) {
-    $scope.subreddit = 'all';
-    $scope.$on('handleSubredditChange', function(e, d){
-      $scope.subreddit = subredditService.subreddit;
-    });
+	$scope.subreddit = 'all';
+	$scope.$on('handleSubredditChange', function(e, d){
+	  $scope.subreddit = subredditService.subreddit;
+	});
   }
 ]);
 
@@ -55,16 +55,16 @@ redditPlusControllers.controller('tabsCtrl', ['$scope', '$log', 'subredditServic
  */
 redditPlusControllers.controller('indexCtrl', ['$scope', '$routeParams', 'Posts', 'titleChangeService', 'subredditService',
   function($scope, $routeParams, Posts, titleChangeService, subredditService) {
-      $scope.posts = Posts.query(function(){
-        titleChangeService.prepTitleChange('r/all');
-        subredditService.prepSubredditChange('all');
-      });
+	  $scope.posts = Posts.query(function(){
+		titleChangeService.prepTitleChange('r/all');
+		subredditService.prepSubredditChange('all');
+	  });
   }
 ]);
 
 redditPlusControllers.controller('identityCtrl', ['$scope', 'identityService', 
   function($scope, identityService){
-    $scope.identity = identityService.query();
+	$scope.identity = identityService.query();
   }]
 );
 
@@ -74,21 +74,21 @@ redditPlusControllers.controller('identityCtrl', ['$scope', 'identityService',
  */
 redditPlusControllers.controller('subredditPostsCtrl', ['$scope', '$routeParams', '$log', 'Posts', 'titleChangeService', 'subredditService',
   function($scope, $routeParams, $log, Posts, titleChangeService, subredditService) {
-    titleChangeService.prepTitleChange('r/' + $routeParams.sub);
-    subredditService.prepSubredditChange($routeParams.sub);
-    Posts.query({sub: $routeParams.sub}, function(data){
-        $scope.posts = data;
-      });
+	titleChangeService.prepTitleChange('r/' + $routeParams.sub);
+	subredditService.prepSubredditChange($routeParams.sub);
+	Posts.query({sub: $routeParams.sub}, function(data){
+		$scope.posts = data;
+	  });
   }
 ]);
 
 redditPlusControllers.controller('subredditPostsSortCtrl', ['$scope', '$routeParams', '$log', 'Posts', 'titleChangeService', 'subredditService',
   function($scope, $routeParams, $log, Posts, titleChangeService, subredditService) {
-    titleChangeService.prepTitleChange('r/' + $routeParams.sub);
-    subredditService.prepSubredditChange($routeParams.sub);
-    Posts.query({sub: $routeParams.sub, sort: $routeParams.sort}, function(data){
-        $scope.posts = data;
-      });
+	titleChangeService.prepTitleChange('r/' + $routeParams.sub);
+	subredditService.prepSubredditChange($routeParams.sub);
+	Posts.query({sub: $routeParams.sub, sort: $routeParams.sort}, function(data){
+		$scope.posts = data;
+	  });
   }
 ]);
 
@@ -98,16 +98,16 @@ redditPlusControllers.controller('subredditPostsSortCtrl', ['$scope', '$routePar
  */
 redditPlusControllers.controller('embedCtrl', ['$scope', '$log',
   function($scope, $log) {
-    
-    $scope.post.showEmbed = false;
-    
-    $scope.show = function() {
-      $scope.post.showEmbed = true;
-    };
+	
+	$scope.post.showEmbed = false;
+	
+	$scope.show = function() {
+	  $scope.post.showEmbed = true;
+	};
 
-    $scope.hide = function() {
-      $scope.post.showEmbed = false;
-    };
+	$scope.hide = function() {
+	  $scope.post.showEmbed = false;
+	};
 
   }
 ]);
@@ -118,28 +118,28 @@ redditPlusControllers.controller('embedCtrl', ['$scope', '$log',
  */
 redditPlusControllers.controller('videoCtrl', ['$scope', '$log',
   function($scope, $log) {
-    
-    $scope.post.showVideo = false;
-    
-    $scope.show = function() {
-      $scope.post.showVideo = true;
-    };
+	
+	$scope.post.showVideo = false;
+	
+	$scope.show = function() {
+	  $scope.post.showVideo = true;
+	};
 
-    $scope.hide = function() {
-      $scope.post.showVideo = false;
-    };
+	$scope.hide = function() {
+	  $scope.post.showVideo = false;
+	};
 
   }
 ]);
 
 redditPlusControllers.controller('tweetCtrl', ['$scope', '$log', 'tweetService', 
   function($scope, $log, tweetService) {
-    $scope.tweet = "";
-    var id = $scope.post.data.url.substring($scope.post.data.url.lastIndexOf('/')+1);
-    var data = tweetService.query({id: id}, function(data){
-      $scope.tweet = data.html;
-      twttr.widgets.load();
-    });
+	$scope.tweet = "";
+	var id = $scope.post.data.url.substring($scope.post.data.url.lastIndexOf('/')+1);
+	var data = tweetService.query({id: id}, function(data){
+	  $scope.tweet = data.html;
+	  twttr.widgets.load();
+	});
   }
 ]);
 
@@ -149,7 +149,7 @@ redditPlusControllers.controller('tweetCtrl', ['$scope', '$log', 'tweetService',
  */
 redditPlusControllers.controller('sidenavSubredditsUserCtrl', ['$scope', 'SubredditsUser',
   function($scope, SubredditsUser){
-    $scope.subs = SubredditsUser.query();
+	$scope.subs = SubredditsUser.query();
   }
 ]);
 
@@ -159,7 +159,7 @@ redditPlusControllers.controller('sidenavSubredditsUserCtrl', ['$scope', 'Subred
  */
 redditPlusControllers.controller('sidenavSubredditsCtrl', ['$scope', 'Subreddits',
   function($scope, Subreddits){
-    $scope.subs = Subreddits.query();
+	$scope.subs = Subreddits.query();
   }
 ]);
 
@@ -168,71 +168,77 @@ redditPlusControllers.controller('sidenavSubredditsCtrl', ['$scope', 'Subreddits
  */
 redditPlusControllers.controller('imgurAlbumCtrl', ['$scope', '$log', '$routeParams', 'imgurAlbumService', 
   function($scope, $log, $routeParams, imgurAlbumService){
-    var imageIndex = 0;
-    $scope.currentImage = 0;
-    $scope.currentImageUrl = "";
-    $scope.imageDescription = "";
-    $scope.imageTitle = "";
+	var imageIndex = 0;
+	$scope.currentImage = 0;
+	$scope.currentImageUrl = "";
+	$scope.imageDescription = "";
+	$scope.imageTitle = "";
 
-    var url = $scope.post.data.url;
+	var url = $scope.post.data.url;
 
-    //get last segment of url and remove unwanted stuff
-    var id = url.substring(url.lastIndexOf('/')+1).replace('?gallery', '').replace('#0', '');
+	//get last segment of url and remove unwanted stuff
+	if (url.indexOf('/gallery/') > 0) {
+	  if (url.indexOf('/new') > 0) {
+		url = url.substring(0, url.indexOf('/new'));
+	  }
+	}
 
-    //set the album info
-    if (id.indexOf(',') > 0) { //implicit album (comma seperated list of image ids)
-      var images = [];
-      var imageIds = id.split(',');
-      imageIds.forEach(function(value, i){
-        images.push({"link" : "http://i.imgur.com/" + value + ".jpg"});
-      });
+	var id = url.substring(url.lastIndexOf('/')+1).replace('?gallery', '').replace('#0', '');
 
-      $scope.album = {
-        "data" : {
-          "images_count": imageIds.length,
-          "images": images
-        }
-      };
-      setCurrentImage();
+	//set the album info
+	if (id.indexOf(',') > 0) { //implicit album (comma seperated list of image ids)
+	  var images = [];
+	  var imageIds = id.split(',');
+	  imageIds.forEach(function(value, i){
+		images.push({"link" : "http://i.imgur.com/" + value + ".jpg"});
+	  });
 
-    } else { //actual album, request album info from api
-      imgurAlbumService.query({id: id}, function(album) {
-        $scope.album = album;
-        setCurrentImage();
-      }, function(error) {
-          var images = [];
-          images[0] = {
-            "link": 'http://i.imgur.com/' + id + '.jpg'
-          }
-          $log.log(images[0]);
-          $scope.album = {
-            "data" : {
-              "images_count": 1,
-              "images": images
-            }
-          };
-          setCurrentImage();
-      });
-    }
+	  $scope.album = {
+		"data" : {
+		  "images_count": imageIds.length,
+		  "images": images
+		}
+	  };
+	  setCurrentImage();
 
-    $scope.prev = function(n) {
-      if(--imageIndex < 0)
-        imageIndex = n-1;
-      setCurrentImage();
-    };
+	} else { //actual album, request album info from api
+	  imgurAlbumService.query({id: id}, function(album) {
+		$scope.album = album;
+		setCurrentImage();
+	  }, function(error) {
+		  var images = [];
+		  images[0] = {
+			"link": 'http://i.imgur.com/' + id + '.jpg'
+		  }
+		  $log.log(images[0]);
+		  $scope.album = {
+			"data" : {
+			  "images_count": 1,
+			  "images": images
+			}
+		  };
+		  setCurrentImage();
+	  });
+	}
 
-    $scope.next = function(n) {
-      if(++imageIndex == n)
-        imageIndex = 0;
-      setCurrentImage();
-    };
+	$scope.prev = function(n) {
+	  if(--imageIndex < 0)
+		imageIndex = n-1;
+	  setCurrentImage();
+	};
 
-    function setCurrentImage() {
-      $scope.currentImageUrl = $scope.album.data.images[imageIndex].link;
-      $scope.imageDescription = $scope.album.data.images[imageIndex].description;
-      $scope.imageTitle = $scope.album.data.images[imageIndex].title;
-      $scope.currentImage = imageIndex+1;
-    }
+	$scope.next = function(n) {
+	  if(++imageIndex == n)
+		imageIndex = 0;
+	  setCurrentImage();
+	};
+
+	function setCurrentImage() {
+	  $scope.currentImageUrl = $scope.album.data.images[imageIndex].link;
+	  $scope.imageDescription = $scope.album.data.images[imageIndex].description;
+	  $scope.imageTitle = $scope.album.data.images[imageIndex].title;
+	  $scope.currentImage = imageIndex+1;
+	}
 
   }
 ]);
@@ -250,60 +256,60 @@ redditPlusControllers.controller('imgurAlbumCtrl', ['$scope', '$log', '$routePar
  */
 redditPlusControllers.controller('progressCtrl', ['$scope', '$rootScope', '$log', '$timeout',
   function($scope, $rootScope, $log, $timeout){
-    $scope.value = 0.2;
-    $scope.loading = true;
-    var incTimeout = 0;
+	$scope.value = 0.2;
+	$scope.loading = true;
+	var incTimeout = 0;
 
-    $rootScope.$on('progressLoading', function(e, d){
-      // $log.log('progressLoading');
-      $scope.loading = true;
-      set(0.2)
-    });
-    $rootScope.$on('progressComplete', function(e,d){
-      // $log.log('progressComplete');
-      set(100);
-      $timeout(function(){
-        $scope.loading = false;
-      }, 600);
-    });
-    $rootScope.$on('progress', function(e, d){
-      // $log.log('progress: ' + d.value);
-      set(d.value);
-    });
+	$rootScope.$on('progressLoading', function(e, d){
+	  // $log.log('progressLoading');
+	  $scope.loading = true;
+	  set(0.2)
+	});
+	$rootScope.$on('progressComplete', function(e,d){
+	  // $log.log('progressComplete');
+	  set(100);
+	  $timeout(function(){
+		$scope.loading = false;
+	  }, 600);
+	});
+	$rootScope.$on('progress', function(e, d){
+	  // $log.log('progress: ' + d.value);
+	  set(d.value);
+	});
 
-    function set(n) {
-      if($scope.loading == false) return;
-      $scope.value = n;
+	function set(n) {
+	  if($scope.loading == false) return;
+	  $scope.value = n;
 
-      $timeout.cancel(incTimeout);
-      incTimeout = $timeout(function(){
-        inc();
-      }, 750);
-    }
+	  $timeout.cancel(incTimeout);
+	  incTimeout = $timeout(function(){
+		inc();
+	  }, 750);
+	}
 
 
-    function inc() {
-      var rnd = 0;
-        // TODO: do this mathmatically instead of through conditions
-        var stat = $scope.value/100;
-        if (stat >= 0 && stat < 0.25) {
-          // Start out between 3 - 6% increments
-          rnd = (Math.random() * (5 - 3 + 1) + 3) / 100;
-        } else if (stat >= 0.25 && stat < 0.65) {
-          // increment between 0 - 3%
-          rnd = (Math.random() * 3) / 100;
-        } else if (stat >= 0.65 && stat < 0.9) {
-          // increment between 0 - 2%
-          rnd = (Math.random() * 2) / 100;
-        } else if (stat >= 0.9 && stat < 0.99) {
-          // finally, increment it .5 %
-          rnd = 0.005;
-        } else {
-          // after 99%, don't increment:
-          rnd = 0;
-        }
-        // $log.log("RANDOM INC: " + rnd*1000);
-        $scope.value += rnd*500;
-    }
+	function inc() {
+	  var rnd = 0;
+		// TODO: do this mathmatically instead of through conditions
+		var stat = $scope.value/100;
+		if (stat >= 0 && stat < 0.25) {
+		  // Start out between 3 - 6% increments
+		  rnd = (Math.random() * (5 - 3 + 1) + 3) / 100;
+		} else if (stat >= 0.25 && stat < 0.65) {
+		  // increment between 0 - 3%
+		  rnd = (Math.random() * 3) / 100;
+		} else if (stat >= 0.65 && stat < 0.9) {
+		  // increment between 0 - 2%
+		  rnd = (Math.random() * 2) / 100;
+		} else if (stat >= 0.9 && stat < 0.99) {
+		  // finally, increment it .5 %
+		  rnd = 0.005;
+		} else {
+		  // after 99%, don't increment:
+		  rnd = 0;
+		}
+		// $log.log("RANDOM INC: " + rnd*1000);
+		$scope.value += rnd*500;
+	}
   }
 ]);
