5ecda339ce6ba117d3ef2e17e51e323d06c0a48c
Jalal Albasri <jalal.albasri@gmail.com>
2015-04-01 13:00:42 +0300

    open time filter select with button

--- public/javascript/timeFilter.js
+++ public/javascript/timeFilter.js
@@ -1,13 +1,5 @@
 jQuery(function(){
-    console.log('bar');
     jQuery('#rp-time-filter-button').on('click', function(){
-        console.log('foo');
-        // jQuery('.rp-time-filter-select').trigger('click');
+        jQuery('.rp-time-filter-select').trigger('click');
     });
-
-    function openSelect() {
-        console.log('jin');
-        // jQuery('.rp-time-filter-select').trigger('click');
-
-    }
 });
