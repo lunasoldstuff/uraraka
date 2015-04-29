b94ea7f6818ac50af526892afc99d3b25bca86a0
CavemanNinja <jalal.albasri@gmail.com>
2015-04-29 17:32:44 +0300

    removed unsed media filters and old rp-media :)

--- public/javascript/ng-app/filters.js
+++ public/javascript/ng-app/filters.js
@@ -12,41 +12,6 @@ redditPlusFilters.filter('subreddit_url', function() {
 	Media and URL related filters.
  */
 
-redditPlusFilters.filter('image_url', function() {
-  return function(data) {
-	var url;
-
-	if (data.url)
-		url = data.url;
-	else
-		url = data;
-
-	if (url.substr(url.length-4) == '.jpg' ||
-	  url.substr(url.length-4) == '.png' ||
-	  url.substr(url.length-4) == '.bmp' ) {
-	  return url;
-	}
-
-	if (data.domain) {
-		if (data.domain.substr(data.domain.length-9) == 'imgur.com') {
-		  url = url.replace('?', '');
-		  if (url.substr(url.length-4) != '.jpg') {
-			if (url.substr(url.length-1) == '/')
-			  url = url.substring(0, url.length-1);
-			return url + '.jpg';
-		  }
-				return url;
-		}
-	}
-
-	if (url.indexOf('zippy.gfycat.com') > 0) {
-		return url.replace('zippy.', '');
-	}
-
-	return url;
-  };
-});
-
 redditPlusFilters.filter('image_url_comment_media', function() {
 	return function(url) {
 		if (url.substr(url.length-4) == '.jpg' ||
@@ -59,73 +24,6 @@ redditPlusFilters.filter('image_url_comment_media', function() {
 	};
 });
 
-redditPlusFilters.filter('video_url', function(){
-  return function(data, type){
-	var url = data.url;
-	var domain = data.domain;
-	var video_url = url;
-
-	if (domain.substr(domain.length-9) == 'imgur.com') {
-	  if(url.substr(url.length-5) == '.gifv')
-		return url.substring(0, url.length-5) + type;
-
-	} else if (domain == 'gfycat.com') {
-		return url.replace('zippy.gfycat.com', 'gfycat.com') + type;
-	} else
-		return url;
-  };
-});
-
-redditPlusFilters.filter('is_video', function() {
-  return function(data) {
-	var url = data.url;
-
-	//check if there is media data and if type is video
-	if (url.substr(url.length-5) == '.gifv')
-	  return true;
-	if (data.media) {
-	  if (data.media.oembed.type == 'video')
-		return true;
-	}
-	return false;
-  };
-});
-
-redditPlusFilters.filter('is_gif', function() {
-  return function(data) {
-	var url = data.url;
-	if (url.indexOf('.gif') > 0 && url.indexOf('.gifv') == -1)
-	  return true;
-	return false;
-  };
-});
-
-redditPlusFilters.filter('thumbnail', function(){
-  return function(data) {
-	if (data.thumbnail)
-	  return data.thumbnail;
-	if (data.media && data.media.oembed.thumbnail_url)
-		return data.media.oembed.thumbnail_url;
-	//if a gfycat thumb still hasn't got a thumbnail_url
-	if (data.domain == "gfycat.com") {
-		var id = data.url.substr(data.url.lastIndexOf('/')+1);
-		if (id.indexOf('.') > 0) {
-			id = id.substring(0, id.indexOf('.'));
-		}
-		return "https://thumbs.gfycat.com/" + id + "-thumb100.jpg";
-	}
-	return '/self';
-  };
-});
-
-/*
-  returns true if the url is an imgur gallery
- */
-redditPlusFilters.filter('is_gallery', function(){
-  return function(url) {
-	return url.indexOf('/a/') > 0 || url.indexOf('/gallery/') > 0 || url.substring(url.lastIndexOf('/')+1).indexOf(',') > 0;
-  };
-});
 
 /*
 	Replaces <a> tags in the comment body with <rp-comment-media> directives.
@@ -213,13 +111,4 @@ redditPlusFilters.filter('rp_media_type', function() {
 			return 'giphy';
 
 	};
-});
-
-redditPlusFilters.filter('rp_media_is_image', function() {
-	return function(url) {
-		return url.substr(url.length-4) == '.jpg' || 
-			url.substr(url.length-4) == '.jpeg' ||
-			url.substr(url.length-4) == '.png' || 
-			url.substr(url.length-4) == '.bmp';
-	}
 });
