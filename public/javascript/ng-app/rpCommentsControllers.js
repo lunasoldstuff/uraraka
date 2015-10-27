311586fd9ff48321b419afbb5df7c2a912a39942
CavemanNinja <jalal.albasri@gmail.com>
2015-10-27 12:22:42 +0300

    added comments controller

--- public/javascript/ng-app/rpCommentsControllers.js
+++ public/javascript/ng-app/rpCommentsControllers.js
@@ -1,1 +1,20 @@
-'use strict'
+'use strict';
+
+var rpCommentsControllers = angular.module('rpCommentsControllers', []);
+
+rpCommentsControllers.controller('rpCommentsCtrl',
+	['$scope',
+	function(
+		$scope
+	) {
+
+		console.log('[rpCommentsCtrl] loaded.');
+
+		
+
+
+
+
+
+	}
+]);
