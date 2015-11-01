4261622d3e4cc219568c2abb4df4c2858638b465
CavemanNinja <jalal.albasri@gmail.com>
2015-11-01 13:26:59 +0300

    Subreddits using react components within ng-repeat directives. Angular directives within react components work fine.

--- public/javascript/ng-app/rpReactComponents.js
+++ public/javascript/ng-app/rpReactComponents.js
@@ -3,72 +3,7 @@ var rpReactComponents = angular.module('rpReactComponents', ['react']);
 /**
  * PINNED SUBREDDITS
  */
-
 var PinnedSubredditsComponent = React.createClass({displayName: "PinnedSubredditsComponent",
-
-	propTypes: {
-		subs: React.PropTypes.array
-	},
-
-	render: function() {
-		
-		var createSubredditItem = function(sub, index) {
-
-			return (
-				React.createElement("md-list-item", {class: "rp-sidenav-subreddit-list-item", key: sub.name}, 
-					React.createElement("md-item-content", {class: "rp-sidenav-subreddit-list-item-content flex"}, 
-						React.createElement("md-button", {class: "rp-sidenav-subreddit-button flex", "data-ng-click": "openSubreddit($event, '" + sub.url + "')"}, sub.name)
-					)
-				)
-
-			);	
-		};
-
-		return React.createElement("md-list", {className: "rp-sidenav-subreddit-list"}, this.props.subs.map(createSubredditItem));
-		
-	}
-
-});
-
-// rpReactComponents.value('PinnedSubredditsComponent', PinnedSubredditsComponent);
-rpReactComponents.directive('pinnedSubredditsComponent', function(reactDirective) {
-	return reactDirective(PinnedSubredditsComponent);
-});
-
-/**
- * SUBREDDITS COMPONENT
- */
-
-var SubredditsComponent = React.createClass({displayName: "SubredditsComponent",
-
-	propTypes: {
-		display_name: React.PropTypes.string,
-		url: React.PropTypes.string
-	},
-
-	render: function() {
-		var createSubredditItem = function(sub, index) {
-
-			return (
-				React.createElement("md-item-content", {class: "rp-sidenav-subreddit-list-item-content flex"}, 
-					React.createElement("md-button", {class: "rp-sidenav-subreddit-button flex", "data-ng-click": "openSubreddit($event, '" + this.props.url + "')"}, 
-						this.props.display_name
-					)
-				)
-			);	
-		};
-
-	}
-
-});
-
-rpReactComponents.value('SubredditsComponent', SubredditsComponent);
-
-/**
- * SUBREDDIT LIST ITEM COMPONENT
- */
-
-var ItemComponent = React.createClass({displayName: "ItemComponent",
 	
 	propTypes: {
 		name: React.PropTypes.string,
@@ -86,25 +21,30 @@ var ItemComponent = React.createClass({displayName: "ItemComponent",
 	}
 });
 
+rpReactComponents.value('PinnedSubredditsComponent', PinnedSubredditsComponent);
 
-
-// rpReactComponents.directive('itemComponent', function(reactDirective) {
-// 	return reactDirective(ItemCommponent);
-// });
-
-rpReactComponents.value('ItemComponent', ItemComponent);
-
-var TestComponent = React.createClass({displayName: "TestComponent",
+/**
+ * SUBREDDITS COMPONENT
+ */
+var SubredditsComponent = React.createClass({displayName: "SubredditsComponent",
 
 	propTypes: {
-	    name: React.PropTypes.string,
-	    url: React.PropTypes.string
+		display_name: React.PropTypes.string,
+		url: React.PropTypes.string
 	},
 
 	render: function() {
-		return React.createElement("li", null, this.props.name);
+
+		return (
+			React.createElement("md-item-content", {class: "rp-sidenav-subreddit-list-item-content flex"}, 
+				React.createElement("md-button", {class: "rp-sidenav-subreddit-button flex", "data-ng-click": "openSubreddit($event, '" + this.props.url + "')"}, 
+					this.props.display_name
+				)
+			)
+		);	
+
 	}
 
 });
 
-rpReactComponents.value('TestComponent', TestComponent);
+rpReactComponents.value('SubredditsComponent', SubredditsComponent);
--- public/javascript/reactComponents/rpReactComponents.jsx
+++ public/javascript/reactComponents/rpReactComponents.jsx
@@ -3,72 +3,7 @@ var rpReactComponents = angular.module('rpReactComponents', ['react']);
 /**
  * PINNED SUBREDDITS
  */
-
 var PinnedSubredditsComponent = React.createClass({
-
-	propTypes: {
-		subs: React.PropTypes.array
-	},
-
-	render: function() {
-		
-		var createSubredditItem = function(sub, index) {
-
-			return (
-				<md-list-item class="rp-sidenav-subreddit-list-item" key={sub.name}>
-					<md-item-content class="rp-sidenav-subreddit-list-item-content flex">
-						<md-button class="rp-sidenav-subreddit-button flex" data-ng-click={"openSubreddit($event, '" + sub.url + "')"}>{sub.name}</md-button>
-					</md-item-content>
-				</md-list-item>
-
-			);	
-		};
-
-		return <md-list className="rp-sidenav-subreddit-list">{this.props.subs.map(createSubredditItem)}</md-list>;
-		
-	}
-
-});
-
-// rpReactComponents.value('PinnedSubredditsComponent', PinnedSubredditsComponent);
-rpReactComponents.directive('pinnedSubredditsComponent', function(reactDirective) {
-	return reactDirective(PinnedSubredditsComponent);
-});
-
-/**
- * SUBREDDITS COMPONENT
- */
-
-var SubredditsComponent = React.createClass({
-
-	propTypes: {
-		display_name: React.PropTypes.string,
-		url: React.PropTypes.string
-	},
-
-	render: function() {
-		var createSubredditItem = function(sub, index) {
-
-			return (
-				<md-item-content class="rp-sidenav-subreddit-list-item-content flex">
-					<md-button class="rp-sidenav-subreddit-button flex" data-ng-click={"openSubreddit($event, '" + this.props.url + "')"}>
-						{this.props.display_name}
-					</md-button>
-				</md-item-content>
-			);	
-		};
-
-	}
-
-});
-
-rpReactComponents.value('SubredditsComponent', SubredditsComponent);
-
-/**
- * SUBREDDIT LIST ITEM COMPONENT
- */
-
-var ItemComponent = React.createClass({
 	
 	propTypes: {
 		name: React.PropTypes.string,
@@ -86,25 +21,30 @@ var ItemComponent = React.createClass({
 	}
 });
 
+rpReactComponents.value('PinnedSubredditsComponent', PinnedSubredditsComponent);
 
-
-// rpReactComponents.directive('itemComponent', function(reactDirective) {
-// 	return reactDirective(ItemCommponent);
-// });
-
-rpReactComponents.value('ItemComponent', ItemComponent);
-
-var TestComponent = React.createClass({
+/**
+ * SUBREDDITS COMPONENT
+ */
+var SubredditsComponent = React.createClass({
 
 	propTypes: {
-	    name: React.PropTypes.string,
-	    url: React.PropTypes.string
+		display_name: React.PropTypes.string,
+		url: React.PropTypes.string
 	},
 
 	render: function() {
-		return <li>{this.props.name}</li>;
+
+		return (
+			<md-item-content class="rp-sidenav-subreddit-list-item-content flex">
+				<md-button class="rp-sidenav-subreddit-button flex" data-ng-click={"openSubreddit($event, '" + this.props.url + "')"}>
+					{this.props.display_name}
+				</md-button>
+			</md-item-content>
+		);	
+
 	}
 
 });
 
-rpReactComponents.value('TestComponent', TestComponent);
+rpReactComponents.value('SubredditsComponent', SubredditsComponent);
