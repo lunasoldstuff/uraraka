28228db0e666fae5c5a31d2f4090c256f0550bca
Jalal Albasri <jalal.albasri@gmail.com>
2016-02-19 18:39:05 +0300

    tring some funky stuff with compiling embed to get iframe directive to work in roMediaDefault embeds

--- public/javascript/ng-app/rpMediaDirectives.js
+++ public/javascript/ng-app/rpMediaDirectives.js
@@ -91,3 +91,25 @@ rpMediaDirectives.directive('rpMediaImgurAlbumWrapper', function() {
 		}
 	};
 });
+
+rpMediaDirectives.directive('rpMediaDefaultEmbed', ['$compile', function($compile) {
+	return {
+		restrict: 'E',
+		scope: {
+			oembed: '=',
+
+		},
+		compile: function(scope, elem) {
+			console.log('[rpMediaDefaultEmbed] compile function, scope.oembed: ' + scope.oembed);
+			console.log('[rpMediaDefaultEmbed] compile function, scope.post.data.media.oembed.html: ' + scope.post.data.media.oembed.html);
+
+			// var el = angular.element(scope.html);
+			// var compiled = $compile(el);
+			// elem.append(el);
+			// compiled(scope);
+		},
+		link: function(scope) {
+			console.log('[rpMediaDefaultEmbed] link, scope.oembed: ' + scope.oembed);
+		},
+	};
+}]);
