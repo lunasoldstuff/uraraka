3c47ef59edbce202cbd72926e8101e28da1a59e5
Jalal Albasri <jalal.albasri@gmail.com>
2016-03-20 12:58:18 +0300

    comment out postmark

--- rpApp/rpMailHandler.js
+++ rpApp/rpMailHandler.js
@@ -1,21 +1,21 @@
-var postmark = require('postmark')(process.env.POSTMARK_API_TOKEN);
+// var postmark = require('postmark')(process.env.POSTMARK_API_TOKEN);
 
 exports.share = function(to, text, subject, callback) {
 
-	console.log('[share] to: ' + to);
-	console.log('[share] subject: ' + subject);
-	console.log('[share] text: ' + text);
-
-	postmark.send({
-		"From": "reddup@reddup.co",
-		"To": to,
-		"Subject": subject,
-		"TextBody": text,
-		"Tag": "share"
-	}, function(err, success) {
-		if (err) callback(err);
-		callback();
-
-	});
+	// console.log('[share] to: ' + to);
+	// console.log('[share] subject: ' + subject);
+	// console.log('[share] text: ' + text);
+	//
+	// postmark.send({
+	// 	"From": "reddup@reddup.co",
+	// 	"To": to,
+	// 	"Subject": subject,
+	// 	"TextBody": text,
+	// 	"Tag": "share"
+	// }, function(err, success) {
+	// 	if (err) callback(err);
+	// 	callback();
+	//
+	// });
 
 };
