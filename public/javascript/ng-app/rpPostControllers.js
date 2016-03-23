3a94ccd1f332179af9ee7dddaef64d87b8c4d2fe
Jalal Albasri <jalal.albasri@gmail.com>
2016-03-23 17:26:38 +0300

    increase load limit obscenely high for a time to put enough content for adsense bot

--- public/javascript/ng-app/rpPostControllers.js
+++ public/javascript/ng-app/rpPostControllers.js
@@ -95,7 +95,7 @@ rpPostControllers.controller('rpPostsCtrl', [
 		var t = $routeParams.t ? $routeParams.t : 'week';
 		var loadingMore = false;
 		$scope.showSub = true;
-		var loadLimit = 12;
+		var loadLimit = 96;
 		var moreLimit = 12;
 
 		for (var i = 0; i < tabs.length; i++) {
