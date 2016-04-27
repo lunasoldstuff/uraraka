d90444687484e5b7403f6f24e15667ada4db6cd4
CavemanNinja <jalal.albasri@gmail.com>
2016-04-27 19:36:25 +0300

    gulpfile improvements

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,16 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var stripDebug = require('gulp-strip-debug');
 var ngmin = require('gulp-ngmin');
+var mainBowerFiles = require('main-bower-files');
+var filter = require('gulp-filter');
+var order = require("gulp-order");
+var cleanCSS = require('gulp-clean-css');
+var cssmin = require('gulp-cssmin');
+var concatCss = require('gulp-concat-css');
 
 // create a default task and just log a message
 gulp.task('default', function() {
-	return gutil.log('Gulp is running!');
+    return gutil.log('Gulp is running!');
 });
 
 // define the default task and add the watch task to it
@@ -20,53 +26,89 @@ gulp.task('default', ['watch']);
 
 // Default task
 gulp.task('watch', function() {
-	// gulp.watch('assets/js/libs/**/*.js', ['squish-jquery']);
-	// gulp.watch('assets/js/*.js', ['build-js']);
-	gulp.watch('public/stylesheets/less/*.less', ['build-css']);
-	gulp.watch('public/javascript/ng-app/*.js', ['build-ng-app']);
+    // gulp.watch('assets/js/libs/**/*.js', ['squish-jquery']);
+    // gulp.watch('assets/js/*.js', ['build-js']);
+    gulp.watch('public/stylesheets/less/*.less', ['build-css']);
+    gulp.watch('public/javascript/ng-app/*.js', ['build-js']);
 });
 
 // Less to CSS: Run manually with: "gulp build-css"
 gulp.task('build-css', function() {
-	//return gulp.src('public/stylesheets/less/*.less')
-	return gulp.src('public/stylesheets/less/style.less')
-		.pipe(plugins.plumber())
-		.pipe(plugins.less())
-		.on('error', function(err) {
-			gutil.log(err);
-			this.emit('end');
-		})
-		.pipe(plugins.autoprefixer({
-			browsers: [
-				'> 1%',
-				'last 2 versions',
-				'firefox >= 4',
-				'safari 7',
-				'safari 8',
-				'IE 8',
-				'IE 9',
-				'IE 10',
-				'IE 11'
-			],
-			cascade: false
-		}))
-		.pipe(plugins.cssmin())
-		.pipe(gulp.dest('public/stylesheets/css')).on('error', gutil.log);
+    //return gulp.src('public/stylesheets/less/*.less')
+
+    var cssFiles = ['public/stylesheets/css/*'];
+
+
+    gulp.src('public/stylesheets/less/style.less')
+        .pipe(plugins.plumber())
+        .pipe(plugins.less())
+        .on('error', function(err) {
+            gutil.log(err);
+            this.emit('end');
+        })
+        .pipe(plugins.autoprefixer({
+            browsers: [
+                '> 1%',
+                'last 2 versions',
+                'firefox >= 4',
+                'safari 7',
+                'safari 8',
+                'IE 8',
+                'IE 9',
+                'IE 10',
+                'IE 11'
+            ],
+            cascade: false
+        }))
+        // .pipe(plugins.cssmin())
+        .pipe(gulp.dest('public/stylesheets/css')).on('error', gutil.log);
+
+    return gulp.src(mainBowerFiles().concat(cssFiles))
+        .pipe(filter('**/*.css'))
+        .pipe(order([
+            'angular-material.css',
+            'font-awesome.css',
+            'normalize.css',
+            'materialdesignicons.css',
+            'mfb.css',
+            'twitter-widget.css',
+            'style.css'
+        ]))
+        .pipe(concatCss('scrolls.min.css'))
+        .pipe(cleanCSS())
+        // .pipe(cssmin())
+        .pipe(gulp.dest('public/stylesheets/dist')).on('error', gutil.log);
+
 });
 
 //prepare ng-app js
-gulp.task('build-ng-app', function() {
-	return gulp.src('public/javascript/ng-app/*.js')
-		// .pipe(ngmin())
-		.pipe(stripDebug())
-		.pipe(concat('rp.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('public/javascript/rp/'));
+gulp.task('build-js', function() {
+
+    var jsFiles = ['public/javascript/ng-app/*', 'public/javascript/resources/*'];
+    var ignoreBowerComponents = ['angular-material'];
+
+    //(http://stackoverflow.com/questions/34547873/exclude-a-folder-from-main-bower-files?lq=1)
+    function mainBowerFilesFilter(filePath) {
+        for (var i = 0; i < ignoreBowerComponents.length; i++) {
+            if (filePath.indexOf(ignoreBowerComponents[i]) !== -1)
+                return false;
+        }
+        return true;
+    }
+
+    return gulp.src(mainBowerFiles({
+            filter: mainBowerFilesFilter
+        }).concat(jsFiles))
+        .pipe(filter('**/*.js'))
+        .pipe(stripDebug())
+        .pipe(concat('spells.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/javascript/dist/'));
 });
 
 function onError(err) {
-	console.log(err);
-	this.emit('end');
+    console.log(err);
+    this.emit('end');
 }
 
 // gulp.task('jshint', function() {
