f20787dce94abf931734eee641564ffb4e2267f2
CavemanNinja <jalal.albasri@gmail.com>
2016-04-27 19:34:53 +0300

    Improved minification and combination of css and js files for better site performance :construction: some font inconsistencies

--- rpApp/rpApp.js
+++ rpApp/rpApp.js
@@ -33,9 +33,10 @@ mongoose.connection.once('open', function(callback) {
 
 // view engine setup
 app.set('views', path.join(__dirname, '/../views'));
-
+app.set('view cache', true);
 app.set('view engine', 'jade');
 
+// set up prerender
 app.use(require('prerender-node').set('prerenderToken', 'ySORarpSlhdHWxklLGVX'));
 
 // uncomment after placing your favicon in /public
