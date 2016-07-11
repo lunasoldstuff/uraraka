38e5a2b68753dbaf5600a3c49a8b74cbd5e57b8c
CavemanNinja <jalal.albasri@gmail.com>
2016-07-11 17:07:46 +0300

    Captcha circular progress hanging use timeout noop

--- public/javascript/ng-app/rpCaptchaControllers.js
+++ public/javascript/ng-app/rpCaptchaControllers.js
@@ -47,7 +47,7 @@ rpCaptchaControllers.controller('rpCaptchaCtrl', ['$scope', '$rootScope', '$time
                     $scope.iden = data.json.data.iden;
                     $scope.captchaImage = 'http://www.reddit.com/captcha/' + $scope.iden + '.png';
                     $scope.showCaptchaProgress = false;
-                    //$timeout(angular.noop, 0);
+                    $timeout(angular.noop, 0);
 
                 }
             });
