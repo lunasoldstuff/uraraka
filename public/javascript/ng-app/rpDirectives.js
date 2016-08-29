402a573bda4a6427075b2c2a0718cdd374ec7c5d
CavemanNinja <jalal.albasri@gmail.com>
2016-08-29 19:26:29 +0300

    use template and inject $templateCache to get animations on the first ng-repeated element to work!

--- public/javascript/ng-app/rpDirectives.js
+++ public/javascript/ng-app/rpDirectives.js
@@ -279,10 +279,10 @@ rpDirectives.directive('rpScore', function() {
     };
 });
 
-rpDirectives.directive('rpLink', function() {
+rpDirectives.directive('rpLink', function($templateCache) {
     return {
         restrict: 'E',
-        templateUrl: 'rpLink.html',
+        template: $templateCache.get('rpLink.html'),
         controller: 'rpLinkCtrl',
         scope: {
             post: '=',
@@ -417,25 +417,36 @@ rpDirectives.directive('rpComment', ['$compile', '$rootScope', 'RecursionHelper'
     };
 }]);
 
-rpDirectives.directive('rpMessageComment', ['$compile', '$rootScope', 'RecursionHelper', function($compile, $rootScope, RecursionHelper) {
-    return {
-        restrict: 'E',
-        replace: true,
-        scope: {
-            parentCtrl: '=',
-            message: '=',
-            depth: '=',
-            identity: '=',
-        },
-        templateUrl: 'rpMessageComment.html',
-        compile: function(element) {
-            return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn) {
+rpDirectives.directive('rpMessageComment', [
+    '$compile',
+    '$rootScope',
+    '$templateCache',
+    'RecursionHelper',
+    function(
+        $compile,
+        $rootScope,
+        $templateCache,
+        RecursionHelper
+    ) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                parentCtrl: '=',
+                message: '=',
+                depth: '=',
+                identity: '=',
+            },
+            template: $templateCache.get('rpMessageComment.html'),
+            compile: function(element) {
+                return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn) {
 
-            });
-        },
-        controller: 'rpMessageCommentCtrl'
-    };
-}]);
+                });
+            },
+            controller: 'rpMessageCommentCtrl'
+        };
+    }
+]);
 
 /*
 	Display links and media in comments.
