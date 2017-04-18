4dce68449d76d408173a2ae043d3f853d8c31179
CavemanNinja <jalal.albasri@gmail.com>
2017-04-18 13:03:14 +0300

    set up new prerender

--- rpApp/rpApp.js
+++ rpApp/rpApp.js
@@ -46,6 +46,9 @@ app.set('views', path.join(__dirname, '/../views'));
 // app.set('view cache', true);
 app.set('view engine', 'pug');
 
+// PRERENDER.IO
+app.use(require('prerender-node').set('prerenderToken', 'rGeAWfVThO6HyXg1eLys'));
+
 
 // favicon
 app.use(favicon(__dirname + '/../public/icons/favicon.ico'));
