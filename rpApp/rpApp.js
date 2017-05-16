a1e095d18883c759d905e8aac6b9abb2c89e2347
Jalal Albasri <jalal.albasri@gmail.com>
2017-05-16 11:58:14 +0300

    force ssl

--- rpApp/rpApp.js
+++ rpApp/rpApp.js
@@ -43,6 +43,14 @@ app.set('views', path.join(__dirname, '/../views'));
 // app.set('view cache', true);
 app.set('view engine', 'pug');
 
+// FORCE SSL
+app.get('*', function (req, res, next) {
+	if (req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
+		res.redirect('https://' + req.hostname + req.url)
+	else
+		next() /* Continue to other routes if we're not redirecting */
+});
+
 // PRERENDER.IO
 app.use(
 	require('prerender-node')
