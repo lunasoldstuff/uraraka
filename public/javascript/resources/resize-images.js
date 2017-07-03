a0f24b63960d7eb08096a4db9cdeaaddb0eb70b6
CavemanNinja <jalal.albasri@gmail.com>
2017-07-03 18:09:19 +0300

    resizing images

---
+++ public/javascript/resources/resize-images.js
@@ -0,0 +1,98 @@
+(function () {
+	console.log('[resize-images]');
+	interact('.resize-drag')
+		// .draggable({
+		// 	onmove: window.dragMoveListener
+		// })
+		.resizable({
+			allowFrom: '[drag-handle]',
+			preserveAspectRatio: true,
+			edges: { left: true, right: true, bottom: true, top: true }
+		})
+		.on('resizestart', function (event) {
+			var target = event.target;
+
+			var width = window.getComputedStyle(target).getPropertyValue('width');
+			console.log('[resize-images] resizestart width: ' + width);
+
+			if (!target.style.originalWidth) {
+				target.style.originalWidth = width;
+			}
+
+			console.log('[resize-images] resizestart target.style.originalWidth: ' + target.style.originalWidth);
+
+		})
+		.on('resizemove', function (event) {
+
+			var target = event.target,
+				x = (parseFloat(target.getAttribute('data-x')) || 0),
+				y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+			//unset the maxwidth
+			target.style.maxWidth = 'none';
+
+
+			// var originalWidth = parseInt(target.style.width);
+			// target.style.orginalWidth = originalWidth
+
+			// update the element's style
+			target.style.width = event.rect.width + 'px';
+			target.style.height = event.rect.height + 'px';
+
+
+			var width = parseFloat(target.style.width);
+			var originalWidth = parseFloat(target.style.originalWidth);
+			var changeX, translateX;
+
+			console.log('[resize-images] resizemove originalWidth: ' + originalWidth + ', width: ' + width);
+			console.log('[resize-images] resizemove changeX: ' + changeX + ', translateX: ' + translateX);
+
+			if (width > originalWidth) {
+				changeX = originalWidth - width;
+				translateX = changeX / 2;
+				target.style.webkitTransform = target.style.transform =
+					'translate(' + translateX + 'px,' + 0 + 'px)';
+
+			}
+
+
+
+
+
+
+			// console.log('[resize-images] target.style.webkitTransform: ' + target.style.webkitTransform);
+
+			// if (changeX < 0) {
+			// 	var translateX = parseInt(targe.style.webkitTransform);
+
+			// 	if (!translateX) {
+			// 		target.style.webkitTransform = target.style.transform =
+			// 			'translateX(' + changeX + 'px)';
+
+			// 	} else {
+			// 		changeX = changeX + translateX;
+			// 		target.style.webkitTransform = target.style.transform =
+			// 			'translateX(' + changeX + 'px)';
+			// 	}
+
+
+			// }
+
+
+			// translate when resizing from top or left edges
+			// x += event.deltaRect.left;
+			// y += event.deltaRect.top;
+
+			// x += event.deltaRect.left;
+			// y += event.deltaRect.top;
+
+			// target.style.webkitTransform = target.style.transform =
+			// 'translate(' + x + 'px,' + y + 'px)';
+
+
+			target.setAttribute('data-x', x);
+			target.setAttribute('data-y', y);
+			target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
+		});
+
+})();
