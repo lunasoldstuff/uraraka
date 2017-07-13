3147d76c094d31551dfa7c9fd3eedbe03fbe842e
CavemanNinja <jalal.albasri@gmail.com>
2017-07-13 13:46:54 +0300

    test shareEmail

--- public/javascript/ng-app/rpShareControllers.js
+++ public/javascript/ng-app/rpShareControllers.js
@@ -370,23 +370,34 @@ rpShareControllers.controller('rpShareEmailFormCtrl', [
 			$scope.showFeedbackAlert = false;
 			$scope.showFeedbackSuccess = false;
 
+			console.log('[rpShareEmailCtrl] $scope.to: ' + $scope.to);
+			console.log('[rpShareEmailCtrl] $scope.shareTitle: ' + $scope.shareTitle);
+			console.log('[rpShareEmailCtrl] $scope.shareLink: ' + $scope.shareLink);
+			console.log('[rpShareEmailCtrl] $scope.identity.name: ' + $scope.identity.name);
+			console.log('[rpShareEmailCtrl] $scope.optionalMessage: ' + $scope.optionalMessage);
+
 			rpShareEmailUtilService($scope.to, $scope.shareTitle, $scope.shareLink, $scope.identity.name, $scope.optionalMessage, function (err, data) {
+
 
 				if (err) {
 					console.log('[rpShareEmailFormCtrl] err');
 					console.log('[rpShareEmailFormCtrl] err: ' + JSON.stringify(err));
 
+					$scope.showFeedback = true;
+					$scope.showFeedbackAlert = true;
+					$scope.showProgress = false;
+					$scope.showButtons = true;
+
+					//Cutom Feedback messages based on error type.
 					//handle recepient email address incorrect
 					if (err.data.message.indexOf('Illegal email address') !== -1) {
-						$scope.showFeedback = true;
 						$scope.feedbackMessage = "Please check the email address provided";
-						$scope.showFeedbackAlert = true;
-						$scope.showProgress = false;
-						$scope.showButtons = true;
+					} else {
+						$scope.feedbackMessage = "Something went wrong sending your email";
 					}
 
 				} else {
-					$scope.feedbackMessage = "Email sent.";
+					$scope.feedbackMessage = "Email sent";
 					$scope.showFeedbackSuccess = true;
 					$scope.showFeedback = true;
 					$scope.showProgress = false;
--- rpApp/rpMailHandler.js
+++ rpApp/rpMailHandler.js
@@ -33,6 +33,7 @@ exports.share = function (to, shareTitle, shareLink, name, optionalMessage, call
     var mail = new helper.Mail(from_email, subject, to_email, content);
 
     var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
     var request = sg.emptyRequest({
         method: 'POST',
         path: '/v3/mail/send',
