77e0ef371496887f980f1b2731ef12192147b773
Jalal Albasri <jalal.albasri@gmail.com>
2018-01-09 13:04:40 +0300

    finished premium sidenav ad

--- public/javascript/ng-app/rpDirectives.js
+++ public/javascript/ng-app/rpDirectives.js
@@ -1065,8 +1065,8 @@ rpDirectives.directive('compile', ['$compile', '$sce',
 		return {
 			link: function(scope, element, attrs) {
 				var ensureCompileRunsOnce = scope.$watch(function(scope) {
-						return $sce.parseAsHtml(attrs.compile)(scope);
-					},
+					return $sce.parseAsHtml(attrs.compile)(scope);
+				},
 					function(value) {
 						// when the parsed expression changes assign it into the current DOM
 						element.html(value);
