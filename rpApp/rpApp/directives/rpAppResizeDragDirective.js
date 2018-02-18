2e2573110e4eca6706108aba3f4729da75b0d2ce
Jalal Albasri <jalal.albasri@gmail.com>
2018-02-18 18:51:47 +0300

    rpAppImgDirective rpAppVideoDirective rpAppResizeDragDirective

---
+++ rpApp/rpApp/directives/rpAppResizeDragDirective.js
@@ -0,0 +1,39 @@
+(function() {
+	'use strict';
+	angular.module('rpApp').directive('rpResizeDrag', [
+		'$rootScope',
+		'mediaCheck',
+		rpResizeDrag
+	]);
+
+	function rpResizeDrag(
+		$rootScope,
+		mediaCheck
+	) {
+		return {
+			restrict: 'C',
+			link: function(scope, elem, attr) {
+				console.log('[rpResizeDrag]');
+
+				mediaCheck.init({
+					scope: scope,
+					media: [{
+						mq: '(max-width: 600px)',
+						enter: function(mq) {
+							elem.removeClass('resize-drag');
+						}
+
+					}, {
+						mq: '(min-width: 601px)',
+						enter: function(mq) {
+							elem.addClass('resize-drag');
+
+						}
+					}]
+				});
+
+			}
+		};
+
+	}
+})();
