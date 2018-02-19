1d5f2e0d66371dda48b3d03697f630af4879647e
Jalal Albasri <jalal.albasri@gmail.com>
2018-02-19 07:36:11 +0300

    move rpSubscribePaypalButton to rpPlusPaypalButton

---
+++ rpApp/rpPlus/directives/rpPlusPaypalButtonDirective.js
@@ -0,0 +1,11 @@
+(function() {
+	'use strict';
+	angular.module('rpPlus').directive('rpPlusPaypalButton', [rpPlusPaypalButton]);
+
+	function rpPlusPaypalButton() {
+		return {
+			restrict: 'E',
+			templateUrl: 'rpPlus/views/rpPlusPaypalButton.html'
+		};
+	}
+})();
