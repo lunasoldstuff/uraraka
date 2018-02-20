c52ca5ebbf42cb6339ac821dc2469dc72c4263c0
Jalal Albasri <jalal.albasri@gmail.com>
2018-02-20 17:54:09 +0300

    rpAppPageContentDirective

--- rpApp/rpDirectives.js
+++ rpApp/rpDirectives.js
@@ -8,65 +8,6 @@ var rpDirectives = angular.module('rpDirectives', []);
 
 
 
-rpDirectives.directive('rpPageContent', ['$rootScope', function($rootScope) {
-	return {
-		restrict: 'C',
-		link: function(scope, element, attrs) {
-
-			var step = 16;
-
-			var deregisterScrollUp = $rootScope.$on('scroll_down', function() {
-				stepDown();
-			});
-
-			var deregisterScrollDown = $rootScope.$on('scroll_up', function() {
-				stepUp();
-			});
-
-			// var deregisterTabsShow = $rootScope.$on('rp_tabs_show', function() {
-			// 	moveUp();
-			// });
-			//
-			// var deregisterTabsHide = $rootScope.$on('rp_tabs_hide', function() {
-			// 	moveDown();
-			// });
-
-			function stepUp() {
-				if (parseInt(element.css('top')) < 0) {
-					element.css('top', '+=' + step);
-				}
-			}
-
-			function stepDown() {
-				if (parseInt(element.css('top')) > -48) {
-					element.css('top', '-=' + step);
-				}
-			}
-
-			function moveUp() {
-				if (parseInt(element.css('top')) < 0) {
-					element.css('top', 0);
-				}
-			}
-
-			function moveDown() {
-				if (parseInt(element.css('top')) > -48) {
-					element.css('top', -48);
-				}
-			}
-
-
-			scope.$on('$destroy', function() {
-				deregisterScrollUp();
-				deregisterScrollDown();
-				// deregisterTabsShow();
-				// deregisterTabsHide();
-			});
-
-		}
-	};
-}]);
-
 rpDirectives.directive('rpSidenavFooter', ['$rootScope', function($rootScope) {
 	return {
 		restrict: 'C',
