fb7705d8475edaa399d6561b1508e8da88e126d6
Jalal Albasri <jalal.albasri@gmail.com>
2018-02-20 18:24:42 +0300

    rpSearchContainsSpacesFilter

--- rpApp/rpFilters.js
+++ rpApp/rpFilters.js
@@ -11,10 +11,10 @@ var rpFilters = angular.module('rpFilters', []);
 
 
 //used by search form to show error message if subreddit contains spaces.
-rpFilters.filter('rp_contains_spaces', [function() {
+rpFilters.filter('rpSearchContainsSpacesFilter', [function() {
 	return function(s) {
 		var spacesRe = /\s/;
-		console.log('[rp_contains_spaces] s: ' + s + ', spacesRe.test(s): ' + spacesRe.test(s));
+		console.log('[rpSearchContainsSpacesFilter] s: ' + s + ', spacesRe.test(s): ' + spacesRe.test(s));
 		return spacesRe.test(s);
 		//alternative
 		// return s.indexOf(' ') !== -1;
---
+++ rpApp/rpSearch/filters/rpSearchContainsSpacesFilter.js
@@ -0,0 +1,15 @@
+(function() {
+	'use strict';
+	angular.module('rpSearch').filter('rpSearchContainsSpacesFilter', [rpSearchContainsSpacesFilter]);
+
+	function rpSearchContainsSpacesFilter() {
+		return function(s) {
+			var spacesRe = /\s/;
+			console.log('[rpSearchContainsSpacesFilter] s: ' + s + ', spacesRe.test(s): ' + spacesRe.test(s));
+			return spacesRe.test(s);
+			//alternative
+			// return s.indexOf(' ') !== -1;
+
+		};
+	}
+})();
