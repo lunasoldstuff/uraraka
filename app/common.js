1d9c688a427fdd8a442e521e00b4b2d41844022b
Jalal Albasri <jalal.albasri@gmail.com>
2018-04-08 16:57:50 +0300

    moved reddit server api tokes to .env. Removed 'pullrequest' configuration

--- app/common.js
+++ app/common.js
@@ -1,6 +1,9 @@
 var env = require('./env.json');
 
 exports.config = function() {
-  var node_env = process.env.NODE_ENV || 'development';
-  return env[node_env];
+	var node_env = process.env.NODE_ENV || 'development';
+	var config = env[node_env];
+	config.oauth.key = process.env.REDDIT_API_KEY;
+	config.oauth.secret = process.env.REDDIT_API_SECRET;
+	return config;
 };
--- app/reddit/redditServer.js
+++ app/reddit/redditServer.js
@@ -1,7 +1,6 @@
 var Snoocore = require('snoocore');
 var when = require('when');
 var open = require('open');
-// var config = require('./config.json');
 var config = require('../common.js').config();
 var crypto = require('crypto');
 var winston = require('winston');
@@ -11,18 +10,6 @@ var redditServer;
 var serverTimeout = 59 * 60 * 1000;
 
 redditServer = new Snoocore(config);
-
-
-// RedditApp.findOne({}, function(err, data){
-//     if (err) throw new error(err);
-//     if (data) {
-//         redditServer.refresh(data.refreshToken).then(function(){
-//             //console.log('We are now authenticated!');
-//         });
-//     } else {
-//         open(redditServer.getExplicitAuthUrl(serverGeneratedState));
-//     }
-// });
 
 exports.getRedditServer = function(req, res, next, callback) {
 	if (redditServer !== null && typeof(redditServer) !== 'undefined') {
