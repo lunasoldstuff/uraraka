e2dcc10d3f59ea3c272919b15637ef87fb410117
Jalal Albasri <jalal.albasri@gmail.com>
2018-04-22 15:56:15 +0300

    twitter keys removed

--- app/twitter/twitterApiHandler.js
+++ app/twitter/twitterApiHandler.js
@@ -1,16 +1,24 @@
 var Twitter = require('twitter');
-var twitterConfig = require('./config.json');
 var winston = require('winston');
 
-var client = new Twitter(twitterConfig);
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET
+});
 
-exports.status = function(id, callback) {
-    client.get('statuses/oembed', {
-        id: id
-    }, function(err, tweet, response) {
-        if (err) {
-            winston.log('error', err);
-        }
-        callback(tweet);
-    });
+exports.status = function (id, callback) {
+  client.get(
+    'statuses/oembed',
+    {
+      id: id
+    },
+    function (err, tweet, response) {
+      if (err) {
+        winston.log('error', err);
+      }
+      callback(tweet);
+    }
+  );
 };
