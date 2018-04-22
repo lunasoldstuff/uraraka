e366ed749820f343dc639ef7544f4bea87fa5e63
Jalal Albasri <jalal.albasri@gmail.com>
2018-04-22 10:49:34 +0300

    set up eslint with airbnb styleguide

--- rpApp/rpNightTheme/controllers/rpNightThemeButtonController.js
+++ rpApp/rpNightTheme/controllers/rpNightThemeButtonController.js
@@ -1,31 +1,31 @@
-(function() {
-	'use strict';
-	angular.module('rpNightTheme').controller('rpNightThemeButtonCtrl', [
-		'$scope',
-		'$rootScope',
-		'rpSettingsService',
-		rpNightThemeButtonCtrl
-	]);
+(function () {
+  'use strict';
 
+  function rpNightThemeButtonCtrl($scope, $rootScope, rpSettingsService) {
+    var deregisterSettingsChanged = $rootScope.$on('rp_settings_changed', function () {
+      $scope.isNightTheme = rpSettingsService.settings.nightTheme;
+    });
 
-	function rpNightThemeButtonCtrl($scope, $rootScope, rpSettingsService) {
-		console.log('[rpNightThemeButtonCtrl] load');
+    console.log('[rpNightThemeButtonCtrl] load');
 
-		$scope.isNightTheme = rpSettingsService.settings.nightTheme;
+    $scope.isNightTheme = rpSettingsService.settings.nightTheme;
 
-		$scope.toggleNightTheme = function() {
-			$scope.isNightTheme = !$scope.isNightTheme;
-			rpSettingsService.setSetting('nightTheme', $scope.isNightTheme);
-		};
+    $scope.toggleNightTheme = function () {
+      $scope.isNightTheme = !$scope.isNightTheme;
+      rpSettingsService.setSetting('nightTheme', $scope.isNightTheme);
+    };
 
-		var deregisterSettingsChanged = $rootScope.$on('rp_settings_changed', function() {
-			$scope.isNightTheme = rpSettingsService.settings.nightTheme;
-		});
+    $scope.$on('$destroy', function () {
+      deregisterSettingsChanged();
+    });
+  }
 
-		$scope.$on('$destroy', function() {
-			deregisterSettingsChanged();
-		});
-
-	}
-
-})();
+  angular
+    .module('rpNightTheme')
+    .controller('rpNightThemeButtonCtrl', [
+      '$scope',
+      '$rootScope',
+      'rpSettingsService',
+      rpNightThemeButtonCtrl
+    ]);
+}());
--- rpApp/rpNightTheme/directives/rpNightThemeButtonDirective.js
+++ rpApp/rpNightTheme/directives/rpNightThemeButtonDirective.js
@@ -1,18 +1,19 @@
-(function() {
-	'use strict';
-	angular.module('rpNightTheme').directive('rpNightThemeButton', [rpNightThemeButton]);
+(function () {
+  'use strict';
 
-	function rpNightThemeButton() {
-		return {
-			restrict: 'E',
-			templateUrl: 'rpNightTheme/views/rpNightThemeButton.html',
-			controller: 'rpNightThemeButtonCtrl',
-			link: function(scope, elem, attrs) {
-				console.log('[rpMightThemeButton] link()');
-				if ('rpToolbarOverflowMenu' in attrs) {
-					elem.find('.md-icon-button').removeClass('md-icon-button');
-				}
-			}
-		};
-	}
-})();
+  function rpNightThemeButton() {
+    return {
+      restrict: 'E',
+      templateUrl: 'rpNightTheme/views/rpNightThemeButton.html',
+      controller: 'rpNightThemeButtonCtrl',
+      link: function (scope, elem, attrs) {
+        console.log('[rpMightThemeButton] link()');
+        if ('rpToolbarOverflowMenu' in attrs) {
+          elem.find('.md-icon-button').removeClass('md-icon-button');
+        }
+      }
+    };
+  }
+
+  angular.module('rpNightTheme').directive('rpNightThemeButton', [rpNightThemeButton]);
+}());
