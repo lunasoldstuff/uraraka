c0ba6a248c16c68ea1b077b15f7f83ad769a62ff
Jalal Albasri <jalal.albasri@gmail.com>
2018-04-22 15:29:12 +0300

    fixed social buttons and support buttons

---
+++ rpApp/rpSocial/directives/rpSocialGithubDirective.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  function rpSocialGithub() {
+    return {
+      restrict: 'E',
+      templateUrl: 'rpSocial/views/rpSocialGithub.html'
+    };
+  }
+  angular.module('rpSocial').directive('rpSocialGithub', [rpSocialGithub]);
+}());
---
+++ rpApp/rpSocial/directives/rpSocialPaypalDirective.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  function rpSocialPaypal() {
+    return {
+      restrict: 'E',
+      templateUrl: 'rpSocial/views/rpSocialPaypal.html'
+    };
+  }
+  angular.module('rpSocial').directive('rpSocialPaypal', [rpSocialPaypal]);
+}());
