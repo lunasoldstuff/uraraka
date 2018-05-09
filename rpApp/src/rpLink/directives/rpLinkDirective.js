4db9d612a085adf901b24ddb8c1aa6758aa39e3d
Jalal Albasri <jalal.albasri@gmail.com>
2018-05-09 18:21:39 +0300

    not necessary to pass in templatecache any more

--- rpApp/src/rpLink/directives/rpLinkDirective.js
+++ rpApp/src/rpLink/directives/rpLinkDirective.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  function rpLink($templateCache) {
+  function rpLink() {
     return {
       restrict: 'E',
       templateUrl: 'rpLink/views/rpLink.html',
@@ -17,5 +17,5 @@
   }
 
   angular.module('rpLink')
-    .directive('rpLink', ['$templateCache', rpLink]);
+    .directive('rpLink', rpLink);
 }());
