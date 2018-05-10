798eae218945e80a563f6fbb553bfe114312ea9b
Jalal Albasri <jalal.albasri@gmail.com>
2018-05-10 19:06:19 +0300

    moved routeChangeListeners that close the sidenavs into the sidenav/sidebar controllers out of rpAppCtrl

--- rpApp/src/rpApp/controllers/rpAppController.js
+++ rpApp/src/rpApp/controllers/rpAppController.js
@@ -25,9 +25,6 @@
 
     var deregisterSlideshowEnd;
     var deregisterSlideshowStart;
-    var deregisterRouteChangeSuccess;
-    var deregisterHandleDescriptionChange;
-    var deregisterHandleTitleChange;
 
     console.log('[rpAppCtrl] $attrs.isAuthenticated: ' + $attrs.isAuthenticated);
     console.log('[rpAppCtrl] $attrs.userAgent: ' + $attrs.userAgent);
@@ -47,21 +44,6 @@
     // init user agent
     rpAppUserAgentService.setUserAgent($attrs.userAgent);
 
-    // TODO: These might be better off in the sidenav controller themselves.
-    function closeSidenavs() {
-      if ($mdSidenav('left')
-        .isOpen()) {
-        $mdSidenav('left')
-          .toggle();
-      }
-
-      if ($mdSidenav('right')
-        .isOpen()) {
-        $mdSidenav('right')
-          .toggle();
-      }
-    }
-
     // TODO: eliminate these events
     deregisterSlideshowStart = $rootScope.$on('rp_slideshow_start', () => {
       console.log('[rpAppCtrl] slideshow start');
@@ -74,15 +56,8 @@
       $timeout(angular.noop, 0);
     });
 
-    deregisterRouteChangeSuccess = $scope.$on('$routeChangeSuccess', function () {
-      console.log('[rpAppCtrl] $routeChangeSuccess');
-      closeSidenavs();
-    });
-
-
     $scope.$on('$destroy', function () {
       deregisterHandleTitleChange();
-      deregisterRouteChangeSuccess();
       deregisterSlideshowEnd();
       deregisterSlideshowStart();
     });
--- rpApp/src/rpSidebar/controllers/rpSidebarButtonController.js
+++ rpApp/src/rpSidebar/controllers/rpSidebarButtonController.js
@@ -3,18 +3,33 @@
 
   function rpSidebarButtonCtrl(
     $scope,
+    $rootScope,
     $mdSidenav
   ) {
-    console.log('[rpSidebarButtonCtrl]');
+    var deregisterRouteChangeSuccess = $scope.$on('$routeChangeSuccess', function () {
+      if ($mdSidenav('right')
+        .isOpen()) {
+        $mdSidenav('right')
+          .toggle();
+      }
+    });
+
     this.toggleSidebar = function () {
       $mdSidenav('right')
         .toggle();
     };
+
+    console.log('[rpSidebarButtonCtrl]');
+
+    $scope.$on('$destroy', () => {
+      deregisterRouteChangeSuccess();
+    });
   }
 
   angular.module('rpSidebar')
     .controller('rpSidebarButtonCtrl', [
       '$scope',
+      '$rootScope',
       '$mdSidenav',
       rpSidebarButtonCtrl
     ]);
