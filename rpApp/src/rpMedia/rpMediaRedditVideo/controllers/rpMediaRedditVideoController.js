dbc4a9987b315f75f09ff1d3f9980f7ed3af6eef
Jalal Albasri <jalal.albasri@gmail.com>
2018-06-04 15:41:47 +0300

    rpMediaRedditVideo support cross posts. do not initialize player until after play.

--- rpApp/src/rpMedia/rpMediaRedditVideo/controllers/rpMediaRedditVideoController.js
+++ rpApp/src/rpMedia/rpMediaRedditVideo/controllers/rpMediaRedditVideoController.js
@@ -1,20 +1,10 @@
 (function () {
   'use strict';
 
-  function rpMediaRedditVideoCtrl(
-    $scope,
-    $window
-  ) {
-    var dashUrl = $scope.post.data.secure_media.reddit_video.dash_url;
-    var corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
-    var url = corsAnywhere.concat(dashUrl);
+  function rpMediaRedditVideoCtrl($scope, $window) {}
 
-    var player = $window.dashjs.MediaPlayer()
-      .create();
-    player.initialize(document.querySelector('#videoPlayer'), url, false);
-  }
-
-  angular.module('rpMediaRedditVideo')
+  angular
+    .module('rpMediaRedditVideo')
     .controller('rpMediaRedditVideoCtrl', [
       '$scope',
       '$window',
--- rpApp/src/rpMedia/rpMediaRedditVideo/directives/rpMediaRedditVideoDirective.js
+++ rpApp/src/rpMedia/rpMediaRedditVideo/directives/rpMediaRedditVideoDirective.js
@@ -1,15 +1,50 @@
 (function () {
   'use strict';
 
-  function rpMediaRedditVideo($window, $sce, $timeout) {
+  function rpMediaRedditVideo($window, $timeout) {
     return {
       restrict: 'E',
       templateUrl: 'rpMedia/rpMediaRedditVideo/views/rpMediaRedditVideo.html',
-      controller: 'rpMediaRedditVideoCtrl'
+      controller: 'rpMediaRedditVideoCtrl',
+      link: function (scope, element, attrs) {
+        $timeout(() => {
+          let dashUrl;
+          if (scope.post.data.crosspost_parent_list) {
+            dashUrl =
+              scope.post.data.crosspost_parent_list[0].secure_media.reddit_video
+                .dash_url;
+          } else {
+            dashUrl = scope.post.data.secure_media.reddit_video.dash_url;
+          }
 
+          console.log(`[rpMediaRedditVideo] link() dashUrl: ${dashUrl}`);
+
+          let corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
+          let url = corsAnywhere.concat(dashUrl);
+          let idSelector = '#video-'.concat(scope.post.data.name);
+          let videoElement = document.querySelector(idSelector);
+          console.log(`[rpMediaRedditVideo] videoElement: ${videoElement}`);
+
+          let player = $window.dashjs.MediaPlayer().create();
+
+          videoElement.addEventListener(
+            'play',
+            () => {
+              console.log('[rpMediaRedditVideo] initialize dash player');
+              player.initialize(videoElement, url, false);
+            },
+            true
+          );
+        }, 0);
+      }
     };
   }
 
-  angular.module('rpMediaRedditVideo')
-    .directive('rpMediaRedditVideo', ['$window', '$sce', '$timeout', rpMediaRedditVideo]);
+  angular
+    .module('rpMediaRedditVideo')
+    .directive('rpMediaRedditVideo', [
+      '$window',
+      '$timeout',
+      rpMediaRedditVideo
+    ]);
 }());
