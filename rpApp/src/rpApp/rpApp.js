01ffa28f5a78c4ad6b6c61cba8551f8eba88fd66
Jalal Albasri <jalal.albasri@gmail.com>
2018-06-05 09:10:58 +0300

    sort modules in rpApp

--- rpApp/src/rpApp/rpApp.js
+++ rpApp/src/rpApp/rpApp.js
@@ -18,63 +18,67 @@ var rpApp = angular.module('rpApp', [
   'angular-google-adsense',
   'angular-inview',
   'youtube-embed',
-  'rpReddit',
-  'rpMedia',
-  'rpIdentity',
-  'rpPost',
-  'rpUser',
-  'rpMessage',
   'rpArticle',
-  'rpSearch',
-  'rpDelete',
-  'rpProgress',
   'rpCaptcha',
   'rpComment',
-  'rpSettings',
-  'rpShare',
-  'rpSubmit',
-  'rpScore',
+  'rpDelete',
+  'rpDialogCloseButton',
   'rpEdit',
+  'rpError',
   'rpFeedback',
+  'rpFormatting',
+  'rpGild',
+  'rpGotoSubreddit',
+  'rpHide',
+  'rpIdentity',
+  'rpLayoutButton',
+  'rpLink',
+  'rpLoginButton',
+  'rpMedia',
+  'rpMessage',
+  'rpNightTheme',
+  'rpOpenNew',
+  'rpOverflowMenu',
+  'rpPost',
+  'rpProgress',
+  'rpReddit',
+  'rpRefreshButton',
   'rpReply',
   'rpSave',
-  'rpHide',
-  'rpOpenNew',
-  'rpGild',
-  'rpLink',
+  'rpScore',
+  'rpSearch',
+  'rpSettings',
+  'rpShare',
+  'rpSidebar',
+  'rpSidenav',
   'rpSlideshow',
-  'rpNightTheme',
-  'rpLoginButton',
+  'rpSocial',
+  'rpSpeedDial',
+  'rpSubmit',
   'rpSubreddits',
+  'rpSubscribe',
+  'rpTemplates',
   'rpToast',
   'rpToolbar',
   'rpToolbarSelect',
-  'rpSubscribe',
-  'rpError',
-  'rpSidebar',
-  'rpSpeedDial',
-  'rpRefreshButton',
-  'rpLayoutButton',
-  'rpDialogCloseButton',
-  'rpGotoSubreddit',
-  'rpOverflowMenu',
-  'rpSocial',
-  'rpSidenav',
-  'rpFormatting',
-  'rpTemplates'
-
+  'rpUser'
 ]);
 
-rpApp.run(['$animate', function ($animate) {
-  $animate.enabled(true);
-}]);
+rpApp.run([
+  '$animate',
+  function ($animate) {
+    $animate.enabled(true);
+  }
+]);
 
 rpApp.constant('angularMomentConfig', {
   preprocess: 'unix',
   timezone: 'utc'
 });
 
-rpApp.config(['$routeProvider', '$locationProvider',
+rpApp.config([
+  '$routeProvider',
+  '$locationProvider',
   function ($routeProvider, $locationProvider) {
     $routeProvider
 
@@ -126,7 +130,8 @@ rpApp.config(['$routeProvider', '$locationProvider',
       })
 
       .when('/message/compose', {
-        templateUrl: 'rpMessage/rpMessageCompose/views/rpMessageComposeCard.html',
+        templateUrl:
+          'rpMessage/rpMessageCompose/views/rpMessageComposeCard.html',
         controller: 'rpMessageComposeCtrl'
       })
 
@@ -221,84 +226,101 @@ rpApp.config(['$routeProvider', '$locationProvider',
 /**
  * Configure Angular Material Themes.
  */
-rpApp.config(['$mdThemingProvider', function ($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    // .primaryPalette('blue')
-    // .primaryPalette('deep-orange')
-    .primaryPalette('indigo')
-    // If you specify less than all of the keys, it will inherit from the
-    // default shades
-    .accentPalette('deep-orange', {
-      default: 'A200'
-    });
+rpApp.config([
+  '$mdThemingProvider',
+  function ($mdThemingProvider) {
+    $mdThemingProvider
+      .theme('default')
+      // .primaryPalette('blue')
+      // .primaryPalette('deep-orange')
+      .primaryPalette('indigo')
+      // If you specify less than all of the keys, it will inherit from the
+      // default shades
+      .accentPalette('deep-orange', {
+        default: 'A200'
+      });
 
-  $mdThemingProvider.theme('indigo')
-    .primaryPalette('indigo')
-    .accentPalette('pink', {
-      default: 'A200'
-    });
-  $mdThemingProvider.theme('green')
-    .primaryPalette('green')
-    .accentPalette('orange', {
-      default: 'A200'
-    });
-  $mdThemingProvider.theme('deep-orange')
-    .primaryPalette('deep-orange')
-    .accentPalette('cyan', {
-      default: '500'
-    });
-  $mdThemingProvider.theme('red')
-    .primaryPalette('red')
-    .accentPalette('blue', {
-      default: 'A200'
-    });
-  $mdThemingProvider.theme('pink')
-    .primaryPalette('pink')
-    .accentPalette('teal', {
-      default: '500'
-    });
-  $mdThemingProvider.theme('purple')
-    .primaryPalette('purple')
-    .accentPalette('teal', {
-      default: '500'
-    });
+    $mdThemingProvider
+      .theme('indigo')
+      .primaryPalette('indigo')
+      .accentPalette('pink', {
+        default: 'A200'
+      });
+    $mdThemingProvider
+      .theme('green')
+      .primaryPalette('green')
+      .accentPalette('orange', {
+        default: 'A200'
+      });
+    $mdThemingProvider
+      .theme('deep-orange')
+      .primaryPalette('deep-orange')
+      .accentPalette('cyan', {
+        default: '500'
+      });
+    $mdThemingProvider
+      .theme('red')
+      .primaryPalette('red')
+      .accentPalette('blue', {
+        default: 'A200'
+      });
+    $mdThemingProvider
+      .theme('pink')
+      .primaryPalette('pink')
+      .accentPalette('teal', {
+        default: '500'
+      });
+    $mdThemingProvider
+      .theme('purple')
+      .primaryPalette('purple')
+      .accentPalette('teal', {
+        default: '500'
+      });
 
-  $mdThemingProvider.alwaysWatchTheme(true);
-}]);
+    $mdThemingProvider.alwaysWatchTheme(true);
+  }
+]);
 
 /**
  * Load SVG sprite sheet
  */
-rpApp.config(['$mdIconProvider', function ($mdIconProvider) {
-  console.log('[rpApp] load svg icon sprite');
-  $mdIconProvider.defaultIconSet('../../icons/sprite/sprite.svg');
-}]);
+rpApp.config([
+  '$mdIconProvider',
+  function ($mdIconProvider) {
+    console.log('[rpApp] load svg icon sprite');
+    $mdIconProvider.defaultIconSet('../../icons/sprite/sprite.svg');
+  }
+]);
 
 /*
   Override $location.path to allow you to change path without reloading.
   http://joelsaupe.com/programming/angularjs-change-path-without-reloading/
  */
-rpApp.run(['$route', '$rootScope', '$location', function ($route, $rootScope, $location) {
-  let original = $location.path;
+rpApp.run([
+  '$route',
+  '$rootScope',
+  '$location',
+  function ($route, $rootScope, $location) {
+    let original = $location.path;
 
-  $location.path = function (path, reload) {
-    console.log('[rpApp rpLocation] path: ' + path + ', reload: ' + reload);
+    $location.path = function (path, reload) {
+      console.log('[rpApp rpLocation] path: ' + path + ', reload: ' + reload);
 
-    if (reload === false) {
-      let lastRoute = $route.current;
+      if (reload === false) {
+        let lastRoute = $route.current;
 
-      console.log('[rpApp rpLocation] LISTENER SET');
+        console.log('[rpApp rpLocation] LISTENER SET');
 
-      let un = $rootScope.$on('$locationChangeSuccess', function () {
-        console.log('[rpApp rpLocation] $locationChangeSuccess (LISTENER UNSET)');
-        $route.current = lastRoute;
-        un();
-      });
-    }
-    return original.apply($location, [path]);
-  };
-}]);
-
+        let un = $rootScope.$on('$locationChangeSuccess', function () {
+          console.log('[rpApp rpLocation] $locationChangeSuccess (LISTENER UNSET)');
+          $route.current = lastRoute;
+          un();
+        });
+      }
+      return original.apply($location, [path]);
+    };
+  }
+]);
 
 // rpApp.config(['$provide', '$httpProvider', function ($provide, $httpProvider) {
 //   $provide.factory('httpInterceptor', function () {
